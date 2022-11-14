c03ddc043bb36bfcc5355b80bd4f3a7c221415b6
Pelusa-not <agustinlorenzatto@gmail.com>
2022-11-14 16:28:03 -0300

    Configurando use en el database

--- servidorSuper/src/dataBase.js
+++ servidorSuper/src/dataBase.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const uri = "mongodb://127.0.0.1/morel";
 
 mongoose.connect(uri,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
 });
 
 const conection = mongoose.connection;
