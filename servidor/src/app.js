3f4eb9a7297d7474994ec5ecfe8ba631ffec9a72
Pelusa <agustinlorenzatto@gmail.com>
2022-12-20 08:03:13 -0300

    En el servidor saque remito y puse vendedores

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const path = require('path');
 
 //settings
 app.set('port',4000);
@@ -15,7 +14,6 @@ app.use('/morel/productos',require('./routes/producto'));
 app.use('/morel/clientes',require('./routes/cliente'));
 app.use('/morel/ventas',require('./routes/venta'));
 app.use('/morel/recibo',require('./routes/recibo'));
-app.use('/morel/remitos',require('./routes/remito'));
 app.use('/morel/movimiento',require('./routes/movProducto'));
 app.use('/morel/compensada',require('./routes/compensada'));
 app.use('/morel/historica',require('./routes/historica'));
@@ -24,5 +22,6 @@ app.use('/morel/rubro',require('./routes/rubro'));
 app.use('/morel/gastos',require('./routes/gasto'));
 app.use('/morel/pedidos',require('./routes/pedido'));
 app.use('/morel/servicios',require('./routes/servicio'));
+app.use('/morel/vendedores',require('./routes/vendedor'));
 
 module.exports = app;
