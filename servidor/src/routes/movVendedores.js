6b513ac2620d5ab19b93259a4ac6848cd163e5b5
Pelusa-not <agustinlorenzatto@gmail.com>
2022-12-29 17:45:02 -0300

    En movimiento de vendedores puse ya para que no se pueda entrar sin permisos y tambien cuando cambie el select o la fecha traiga lo correcto

--- servidor/src/routes/movVendedores.js
+++ servidor/src/routes/movVendedores.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 
 const router = Router();
-const {post, getAll} = require('../controllers/movVendedores.controllers')
+const {post, getAll, getForNameAndDate} = require('../controllers/movVendedores.controllers')
 
 router.route('/')
     .post(post)
     .get(getAll)
-
+router.route('/:desde/:name')
+    .get(getForNameAndDate)
 module.exports = router;
