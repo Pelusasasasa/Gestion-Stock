e25218d2d2fcffc5fcd65b6b49650a5b1a2cd2de
Pelusa <agustinlorenzatto@gmail.com>
2023-02-07 08:37:29 -0300

    Puse para que se vean los presupuestos

--- gestion/src/index.js
+++ gestion/src/index.js
@@ -140,7 +140,7 @@ const hacerMenu = () => {
         {
           label:"Numeros",
           click(){
-            abrirVentana("numeros/numeros.html",550,700)
+            abrirVentana("numeros/numeros.html",750,700)
           }
         },
         {
