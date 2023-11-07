5a244dbd299a69700152345af4f44892c967bc7b
Pelusa-not <agustinlorenzatto@gmail.com>
2023-11-07 18:22:05 -0300

    Dividi en movineintos para se que se diferencia el contado con el cuenta corriente

--- gestion/src/movimiento/movimiento.js
+++ gestion/src/movimiento/movimiento.js
@@ -5,18 +5,25 @@ const { clickderecho } = require('../helpers');
 require("dotenv").config();
 const URL = process.env.GESTIONURL;
 
+const contado = document.getElementById('contado');
+const cuentaCorriente = document.getElementById('cuentaCorriente');
+
+
 const select = document.querySelector('#rubro');
 const tbody = document.querySelector('tbody');
 const desde = document.querySelector('#desde');
 const hasta = document.querySelector('#hasta');
 const esteMes = document.querySelector('.mes');
 const total = document.querySelector('#total');
+const totalCuentaCorriente = document.querySelector('#totalCuentaCorriente');
 
 const volver = document.querySelector('.volver');
 
-
 let seleccionado;
 let subSeleccionado;
+let contadoBandera = true;
+let totalContado;
+let totalCorriente
 
 const hoy = new Date();
 let dia = hoy.getDate();
@@ -41,11 +48,30 @@ window.addEventListener('load',async e => {
     traerProductos(select.value);
 });
 
+cuentaCorriente.addEventListener('click',e =>{
+    if (contado.classList.contains('active')) {
+        cuentaCorriente.classList.add('active');
+        contado.classList.remove('active');
+        contadoBandera = false;
+        traerProductos(select.value);
+    }
+});
+
+contado.addEventListener('click',e =>{
+    if (cuentaCorriente.classList.contains('active')) {
+        contado.classList.add('active');
+        cuentaCorriente.classList.remove('active');
+        contadoBandera = true;
+        traerProductos(select.value);
+    }
+});
+
 const traerProductos = async(rubro,mesBandera)=>{
     tbody.innerHTML = ""; //LIMPIAMOS EL TBODY
     let totalMovimientos = 0; //ES UN TOTAL PARA  MOSTRAR EN LA PANTALLA
+    let auxCuentaCorriente = 0;
     let movimiento;
-    console.log(mesBandera)
+
     if (mesBandera) {
         const primerDiaMes = new Date();
         primerDiaMes.setDate(1);
@@ -126,12 +152,19 @@ const traerProductos = async(rubro,mesBandera)=>{
         tr.appendChild(tdTotal);
         tr.appendChild(tdMarca);
 
-        tbody.appendChild(tr);
+        if ((mov.tipo_venta !== "CC" && contadoBandera) || (mov.tipo_venta === "CC" && !contadoBandera)) {
+            tbody.appendChild(tr);
+        }
 
-        totalMovimientos += mov.cantidad * mov.precio;//sumamos el total del movimiento al total para despues mostrarlo
+        if (mov.tipo_venta !== "CC") {
+            totalMovimientos += mov.cantidad * mov.precio;//sumamos el total del movimiento al total para despues mostrarlo
+        }else{
+            auxCuentaCorriente += mov.cantidad * mov.precio;
+        }
     };
 
     total.value = totalMovimientos.toFixed(2);
+    totalCuentaCorriente.value = auxCuentaCorriente.toFixed(2);
 };
 
 hasta.addEventListener('keypress',e=>{
