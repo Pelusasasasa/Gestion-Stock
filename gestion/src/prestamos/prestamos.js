ecb4a2b0a7e7b7d846d04b4522d7f66cfb429a70
Pelusa-not <agustinlorenzatto@gmail.com>
2024-07-23 10:10:56 -0300

    Hice para que se peuda ver los movimientos de los prestamos sin el precio actualizado

--- gestion/src/prestamos/prestamos.js
+++ gestion/src/prestamos/prestamos.js
@@ -6,11 +6,146 @@ const URL = process.env.URL;
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 
+const tbodyMovimientos = document.querySelector('#tbody-movimientos');
+
 let seleccionado = '';
 let subSeleccionado = '';
-
 let prestamos = [];
 
+//Funciones
+const listarPrestamos = async(lista) => {
+    
+    for(let elem of lista){
+        const tr = document.createElement('tr');
+        tr.id = elem.nro_comp;
+        
+        const tdFecha = document.createElement('td');
+        const tdIdCliente = document.createElement('td');
+        const tdCliente = document.createElement('td');
+        const tdNumero = document.createElement('td');
+        const tdObservaciones = document.createElement('td');
+        
+
+        tdFecha.innerText = elem.fecha.slice(0,10).split('-',3).reverse().join('/');
+        tdIdCliente.innerText = elem.codigo;
+        tdCliente.innerText = elem.cliente;
+        tdNumero.innerText = elem.nro_comp;
+        tdObservaciones.innerText = elem.observaciones;
+
+
+        tr.appendChild(tdFecha);
+        tr.appendChild(tdIdCliente);
+        tr.appendChild(tdCliente);
+        tr.appendChild(tdNumero);
+        tr.appendChild(tdObservaciones);
+
+        tbody.appendChild(tr);
+    }
+
+};
+
+const listarMovimientos = async( num ) => {
+    const movimientos = (await axios.get(`${URL}movProductos/${num}/Prestamo`)).data;
+    tbodyMovimientos.innerHTML = '';
+    tbody.parentElement.parentElement.style.height = '40vh';
+
+    seleccionado.scrollIntoView({
+        block:'center',
+        behavior:'smooth'
+    });
+
+    
+    for(let mov of movimientos){
+        const tr = document.createElement('tr');
+        tr.id = mov._id;
+
+        const tdCodigo = document.createElement('td');
+        const tdDescripcion = document.createElement('td');
+        const tdCantidad = document.createElement('td');
+        const tdStock = document.createElement('td');
+        const tdPrecio = document.createElement('td');
+
+        tdCodigo.innerText = mov.codProd;
+        tdDescripcion.innerText = mov.descripcion;
+        tdCantidad.innerText = mov.egreso.toFixed(2);
+        tdStock.innerText = mov.stock.toFixed(2);
+        tdPrecio.innerText = mov.precio_unitario.toFixed(2);
+
+        tr.appendChild(tdCodigo);
+        tr.appendChild(tdDescripcion);
+        tr.appendChild(tdCantidad);
+        tr.appendChild(tdStock);
+        tr.appendChild(tdPrecio);
+
+        tbodyMovimientos.appendChild(tr);
+    };
+};
+
+const exportarExcel = async() => {
+    const prestamosAux = prestamos.filter( prestamo => prestamo.observaciones === seleccionado.children[4].innerText);
+    const movimientosAux = [];
+    let totalAux = 0;
+
+    for(let elem of prestamosAux){
+        const mov = (await axios.get(`${URL}movProductos/${elem.nro_comp}/Prestamo`)).data;
+        movimientosAux.push(...mov);
+    };
+
+    for(let elem of movimientosAux){
+        const pro = (await axios.get(`${URL}productos/${elem.codProd}`)).data;
+        elem.precio_unitario = pro.oferta ? pro.precio_oferta : pro.precio_venta;
+        elem.total = (elem.egreso * elem.precio_unitario);
+        totalAux += parseFloat(elem.total);
+    }
+
+    const XLSX = require('xlsx');
+
+    if (!seleccionado) return;
+
+    let path = await ipcRenderer.invoke('saveDialog');
+    let wb = XLSX.utils.book_new();
+
+    let extencion = 'xlsx';
+    
+    let resultante = [];
+
+    movimientosAux.forEach( mov => {
+        const obj = {};
+        obj.fecha = mov.fecha.slice(0,10).split('-',3).reverse().join('/');
+        obj.codigo = mov.codProd;
+        obj.descripcion = mov.descripcion;
+        obj.nro_comp = mov.nro_comp
+        obj.cantidad = mov.egreso.toFixed(2);
+        obj.precio = mov.precio_unitario.toFixed(2);
+        obj.total = mov.total.toFixed(2);
+        obj.vendedor = mov.vendedor;
+
+        resultante.push(obj);
+    });
+
+    resultante.push({
+        '': '',
+        '': '',
+        'TOTAL':totalAux
+    })
+
+    wb.props = {
+        Title: 'Movimientos',
+        subject: 'Movimientos',
+        Author: 'Electro Avenida'
+    };
+
+    let newWs = XLSX.utils.json_to_sheet(resultante);
+
+    XLSX.utils.book_append_sheet(wb, newWs, "Movimientos");
+
+    XLSX.writeFile(wb, path + "." + extencion);
+
+
+
+};
+
+//Eventos
 window.addEventListener('load', async () => {
     prestamos = (await axios.get(`${URL}prestamos/noAnulados`)).data.filter(prestamo => prestamo.codigo === "L083");
     listarPrestamos(prestamos);
@@ -90,7 +225,11 @@ tbody.addEventListener('click', e => {
 
         subSeleccionado = e.target;
         subSeleccionado.classList.add('subSeleccionado');
-    }
+    };
+
+
+    tbodyMovimientos.parentElement.parentElement.classList.remove('none');
+    listarMovimientos(seleccionado.id);
 
 });
 
@@ -100,104 +239,7 @@ tbody.addEventListener('dblclick', e => {
     
 });
 
-const listarPrestamos = async(lista) => {
-    
-    for(let elem of lista){
-        const tr = document.createElement('tr');
-        tr.id = elem.nro_comp;
-        
-        const tdFecha = document.createElement('td');
-        const tdIdCliente = document.createElement('td');
-        const tdCliente = document.createElement('td');
-        const tdNumero = document.createElement('td');
-        const tdObservaciones = document.createElement('td');
-        
-
-        tdFecha.innerText = elem.fecha.slice(0,10).split('-',3).reverse().join('/');
-        tdIdCliente.innerText = elem.codigo;
-        tdCliente.innerText = elem.cliente;
-        tdNumero.innerText = elem.nro_comp;
-        tdObservaciones.innerText = elem.observaciones;
-
-
-        tr.appendChild(tdFecha);
-        tr.appendChild(tdIdCliente);
-        tr.appendChild(tdCliente);
-        tr.appendChild(tdNumero);
-        tr.appendChild(tdObservaciones);
-
-        tbody.appendChild(tr);
-    }
-
-};
-
-const exportarExcel = async() => {
-    const prestamosAux = prestamos.filter( prestamo => prestamo.observaciones === seleccionado.children[4].innerText);
-    const movimientosAux = [];
-    let totalAux = 0;
-
-    for(let elem of prestamosAux){
-        const mov = (await axios.get(`${URL}movProductos/${elem.nro_comp}/Prestamo`)).data;
-        movimientosAux.push(...mov);
-    };
-
-    for(let elem of movimientosAux){
-        const pro = (await axios.get(`${URL}productos/${elem.codProd}`)).data;
-        elem.precio_unitario = pro.oferta ? pro.precio_oferta : pro.precio_venta;
-        elem.total = (elem.egreso * elem.precio_unitario);
-        totalAux += parseFloat(elem.total);
-    }
-
-    const XLSX = require('xlsx');
-
-    if (!seleccionado) return;
-
-    let path = await ipcRenderer.invoke('saveDialog');
-    let wb = XLSX.utils.book_new();
-
-    let extencion = 'xlsx';
-    
-    let resultante = [];
-
-    movimientosAux.forEach( mov => {
-        const obj = {};
-        obj.fecha = mov.fecha.slice(0,10).split('-',3).reverse().join('/');
-        obj.codigo = mov.codProd;
-        obj.descripcion = mov.descripcion;
-        obj.nro_comp = mov.nro_comp
-        obj.cantidad = mov.egreso.toFixed(2);
-        obj.precio = mov.precio_unitario.toFixed(2);
-        obj.total = mov.total.toFixed(2);
-        obj.vendedor = mov.vendedor;
-
-        resultante.push(obj);
-    });
-
-    resultante.push({
-        '': '',
-        '': '',
-        'TOTAL':totalAux
-    })
-
-    wb.props = {
-        Title: 'Movimientos',
-        subject: 'Movimientos',
-        Author: 'Electro Avenida'
-    };
-
-    let newWs = XLSX.utils.json_to_sheet(resultante);
-
-    XLSX.utils.book_append_sheet(wb, newWs, "Movimientos");
-
-    XLSX.writeFile(wb, path + "." + extencion);
-
-
-
-}
-
-
 document.addEventListener('keydown', e => {
-    console.log(e.keyCode)
     if (e.keyCode === 27) {
         window.location = `../menu.html`;
     }
