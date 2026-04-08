26a55ba49cf6f3c7e8f54e42fed76ffca218010f
Pelusa-not <agustinlorenzatto@outlook.com>
2026-04-08 17:02:42 -0300

    Arreglando error de descontar stock

--- servidor/src/controllers/venta.controllers.js
+++ servidor/src/controllers/venta.controllers.js
@@ -1,16 +1,14 @@
 const ventaCTRL = {};
 
-const Venta = require("../models/Venta");
-const funcion = require("../assets/js/pdf");
-const { cambiarSaldoCliente } = require("../helpers/cambiarSaldoCliente");
-const { actualizarNumero } = require("../helpers/actualizarNumero");
-const { descontarStock } = require("../helpers/descontarStock");
-const { crearMovimientosStock } = require("../helpers/crearMovimientosStock");
-const { crearCompensada } = require("../helpers/crearCompensada");
-const { crearHistorica } = require("../helpers/crearHistorica");
-const {
-  crearMovimientoVendedores,
-} = require("../helpers/crearMovimientoVendedores");
+const Venta = require('../models/Venta');
+const funcion = require('../assets/js/pdf');
+const { cambiarSaldoCliente } = require('../helpers/cambiarSaldoCliente');
+const { actualizarNumero } = require('../helpers/actualizarNumero');
+const { descontarStock } = require('../helpers/descontarStock');
+const { crearMovimientosStock } = require('../helpers/crearMovimientosStock');
+const { crearCompensada } = require('../helpers/crearCompensada');
+const { crearHistorica } = require('../helpers/crearHistorica');
+const { crearMovimientoVendedores } = require('../helpers/crearMovimientoVendedores');
 
 ventaCTRL.getForId = async (req, res) => {
   const { id, tipoVenta } = req.params;
@@ -18,7 +16,7 @@ ventaCTRL.getForId = async (req, res) => {
   try {
     const venta = await Venta.findOne({
       $and: [{ tipoVenta: tipoVenta }, { numero: id }],
-    }).populate("vendedor", "nombre");
+    }).populate('vendedor', 'nombre');
 
     res.status(200).json({
       ok: true,
@@ -28,7 +26,7 @@ ventaCTRL.getForId = async (req, res) => {
     console.error(error);
     res.status(500).json({
       ok: false,
-      msg: "No se pudo obtener la venta, hable con el administrador",
+      msg: 'No se pudo obtener la venta, hable con el administrador',
     });
   }
 };
@@ -37,10 +35,7 @@ ventaCTRL.putForId = async (req, res) => {
   const { id, tipo } = req.params;
   try {
     delete req.body._id;
-    const venta = await Venta.findOneAndUpdate(
-      { numero: id, tipo_venta: tipo },
-      req.body,
-    );
+    const venta = await Venta.findOneAndUpdate({ numero: id, tipo_venta: tipo }, req.body);
     res.status(200).json({
       ok: true,
       venta,
@@ -49,7 +44,7 @@ ventaCTRL.putForId = async (req, res) => {
     console.error(error);
     res.status(500).json({
       ok: false,
-      msg: "No se pudo actualizar la venta, hable con el administrador",
+      msg: 'No se pudo actualizar la venta, hable con el administrador',
     });
   }
 };
@@ -63,52 +58,44 @@ ventaCTRL.cargarVenta = async (req, res) => {
       venta.numero = numeroActualizado.numero;
     }
 
-    if (venta.tipo_venta === "CC") {
-      const saldoModficado = await cambiarSaldoCliente(
-        venta.idCliente,
-        venta.precio,
-        false,
-        venta.tipo_comp,
-      );
+    if (venta.tipo_venta === 'CC') {
+      const saldoModficado = await cambiarSaldoCliente(venta.idCliente, venta.precio, false, venta.tipo_comp);
 
       if (!saldoModficado.ok)
         return res.status(400).json({
-          msg: "Error al modificar el saldo del cliente",
+          msg: 'Error al modificar el saldo del cliente',
           ok: false,
         });
 
       const compensada = await crearCompensada(venta);
       if (!compensada)
         return res.status(400).json({
-          msg: "Error al crear la compensada",
+          msg: 'Error al crear la compensada',
           ok: false,
         });
 
       const historica = await crearHistorica(venta);
       if (!historica)
         return res.status(400).json({
-          msg: "Error al crear la historica",
+          msg: 'Error al crear la historica',
           ok: false,
         });
     }
 
-    if (venta.tipo_venta !== "PP" && req.body.esRemito !== true) {
-      const stockDescontado = await descontarStock(req.body.listaProductos);
+    if (venta.tipo_venta !== 'PP' && req.body.esRemito !== true) {
+      const stockDescontado = await descontarStock(req.body.listaProductos, venta.vendedor, venta.numero);
       if (!stockDescontado)
         return res.status(400).json({
           ok: false,
-          msg: "Error al descontar el stock",
+          msg: 'Error al descontar el stock',
         });
     }
 
-    const movimientos = await crearMovimientosStock(
-      req.body.listaProductos,
-      venta,
-    );
+    const movimientos = await crearMovimientosStock(req.body.listaProductos, venta);
     if (!movimientos)
       return res.status(400).json({
         ok: false,
-        msg: "Error al crear los movimientos",
+        msg: 'Error al crear los movimientos',
       });
 
     await venta.save();
@@ -116,15 +103,9 @@ ventaCTRL.cargarVenta = async (req, res) => {
       funcion.crearPDF(req.body); //creamos un pdf con la venta
     }
 
-    await crearMovimientoVendedores(
-      `Se hizo una venta al cliente ${venta.cliente}`,
-      venta.vendedor,
-    );
+    await crearMovimientoVendedores(`Se hizo una venta al cliente ${venta.cliente}`, venta.vendedor);
 
-    const nuevaVenta = await Venta.findOne({ _id: venta._id }).populate(
-      "vendedor",
-      "nombre",
-    );
+    const nuevaVenta = await Venta.findOne({ _id: venta._id }).populate('vendedor', 'nombre');
 
     console.log(`Venta con el numero: ${venta.numero} Cargada`);
     res.status(201).json({
@@ -136,7 +117,7 @@ ventaCTRL.cargarVenta = async (req, res) => {
     console.error(error);
     res.status(500).json({
       ok: false,
-      msg: "Error al cargar la venta, hable con el administrador",
+      msg: 'Error al cargar la venta, hable con el administrador',
     });
   }
 };
@@ -151,7 +132,7 @@ ventaCTRL.VentasDia = async (req, res) => {
 
     const ventas = await Venta.find({
       $and: [{ fecha: { $gte: inicioDia } }, { fecha: { $lte: finDia } }],
-    }).populate("vendedor", "nombre");
+    }).populate('vendedor', 'nombre');
 
     console.log(ventas);
 
@@ -160,7 +141,7 @@ ventaCTRL.VentasDia = async (req, res) => {
     console.error(error);
     res.status(500).json({
       ok: false,
-      msg: "No se pudieron obtener las ventas, hable con el administrador",
+      msg: 'No se pudieron obtener las ventas, hable con el administrador',
     });
   }
 };
@@ -172,18 +153,11 @@ ventaCTRL.ventasMes = async (req, res) => {
     mes = mes > 12 ? 1 : mes;
     let hoy = new Date();
     let fechaConMes = new Date(`${hoy.getFullYear()}-${mes}-1`);
-    let fechaConMesSig = new Date(
-      `${mes === 12 ? hoy.getFullYear() + 1 : hoy.getFullYear()}-${
-        mes === 12 ? 1 : mes + 1
-      }-1`,
-    );
+    let fechaConMesSig = new Date(`${mes === 12 ? hoy.getFullYear() + 1 : hoy.getFullYear()}-${mes === 12 ? 1 : mes + 1}-1`);
 
     const ventas = await Venta.find({
-      $and: [
-        { fecha: { $gte: new Date(fechaConMes) } },
-        { fecha: { $lte: new Date(fechaConMesSig) } },
-      ],
-    }).populate("vendedor", "nombre");
+      $and: [{ fecha: { $gte: new Date(fechaConMes) } }, { fecha: { $lte: new Date(fechaConMesSig) } }],
+    }).populate('vendedor', 'nombre');
 
     res.status(200).json({
       ok: true,
@@ -193,7 +167,7 @@ ventaCTRL.ventasMes = async (req, res) => {
     console.error(error);
     res.status(500).json({
       ok: false,
-      msg: "No se pudieron obtener las ventas, hable con el administrador",
+      msg: 'No se pudieron obtener las ventas, hable con el administrador',
     });
   }
 };
@@ -217,17 +191,14 @@ ventaCTRL.deleteForId = async (req, res) => {
     if (!venta)
       return res.status(404).json({
         ok: false,
-        msg: "Venta no encontrada",
+        msg: 'Venta no encontrada',
       });
 
-    const movCreado = await crearMovimientoVendedores(
-      `Elimino la venta con numero ${venta.numero}`,
-      req.query.vendedor,
-    );
+    const movCreado = await crearMovimientoVendedores(`Elimino la venta con numero ${venta.numero}`, req.query.vendedor);
     if (!movCreado)
       return res.status(500).json({
         ok: false,
-        msg: "No se pudo crear el movimiento de vendedor, Hable con el administrador",
+        msg: 'No se pudo crear el movimiento de vendedor, Hable con el administrador',
       });
 
     res.status(200).json({
@@ -238,7 +209,7 @@ ventaCTRL.deleteForId = async (req, res) => {
     console.error(error);
     res.status(500).json({
       ok: false,
-      msg: "No se pudo eliminar la venta, hable con el administrador",
+      msg: 'No se pudo eliminar la venta, hable con el administrador',
     });
   }
 };
@@ -251,14 +222,10 @@ ventaCTRL.getForNumberAndType = async (req, res) => {
 
 ventaCTRL.getbetweenDate = async (req, res) => {
   const { desde, hasta } = req.params;
-  const aux = hasta.split("-", 3);
+  const aux = hasta.split('-', 3);
   let finDia = new Date(aux[0], aux[1] - 1, aux[2], 20, 59, 59, 0);
   const ventas = await Venta.find({
-    $and: [
-      { fecha: { $gte: desde } },
-      { fecha: { $lte: finDia } },
-      { tipo_comp: { $ne: "Comprobante" } },
-    ],
+    $and: [{ fecha: { $gte: desde } }, { fecha: { $lte: finDia } }, { tipo_comp: { $ne: 'Comprobante' } }],
   });
   res.send(ventas);
 };
@@ -267,13 +234,13 @@ ventaCTRL.getPorFactura = async (req, res) => {
   try {
     const { factura, tipo } = req.params;
     const venta = await Venta.findOne({
-      $and: [{ "afip.numero": parseInt(factura) }, { tipo_comp: tipo }],
+      $and: [{ 'afip.numero': parseInt(factura) }, { tipo_comp: tipo }],
     });
 
     if (!venta)
       return res.status(404).json({
         ok: false,
-        msg: "Factura no encontrada",
+        msg: 'Factura no encontrada',
       });
 
     res.status(200).json({
@@ -284,7 +251,7 @@ ventaCTRL.getPorFactura = async (req, res) => {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: "error al obtener la factura hable con el administrador",
+      msg: 'error al obtener la factura hable con el administrador',
     });
   }
 };
